9c627715ee7d91349dbf1b38b256985baa5a1ad6
rousolalrousan <rousolalrousan9@gmail.com>
2026-09-15 03:50:50 +0300

    Add more student information

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 name = "rousol alrousan"
 major = "Artificial Intelligence"
 university = "jordan university of science and technology"
+graduation_year = 2029
+
 
 print("Student Profile")
 print("----------------")
 print("Name:", name)
 print("Major:", major)
 print("University:", university)
+print("Graduation Year:", graduation_year)
