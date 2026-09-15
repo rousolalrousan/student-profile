092ab281cb264844b39ec7d0ca25c391c9569002
rousolalrousan <rousolalrousan9@gmail.com>
2026-09-15 03:47:59 +0300

    Create student profile

---
+++ main.py
@@ -0,0 +1,9 @@
+name = "rousol alrousan"
+major = "Artificial Intelligence"
+university = "jordan university of science and technology"
+
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
